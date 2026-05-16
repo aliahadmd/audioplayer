549504d6a796c8d775134db4ea2f753c60328ba2
Ali <aliahadmd1@outlook.com>
2026-05-16 17:51:27 +0800

    chore: bump Android release version to 0.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,8 +45,8 @@ android {
         applicationId = "me.aliahad.audioplayer"
         minSdk = 31
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.0.1"
+        versionCode = 2
+        versionName = "0.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
